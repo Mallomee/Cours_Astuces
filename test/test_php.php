766af3e88ad7d520f19efc29fac06f567fb86b9c
Mallomee <78727080+Mallomee@users.noreply.github.com>
2021-04-07 18:43:21 +0200

    test_maj

--- test/test_php.php
+++ test/test_php.php
@@ -1,9 +1,23 @@
 <?php 
+// Voici un exemple de calcul simple
 
+// $a est un int
 $a = 3;
+// $b est un float
 $b = 3.5;
+// $calcul est la somme de $a + $b
 $calcul = $a + $b;
-
+// Grâce à echo, on affiche le résultat sur la page
 echo $calcul;
+
+// Ici voici deux exemples de texte l'un entre " " et l'autre entre ' ' avec une variable incluse pour comprendre la différence mmême si elle est déjà visible dans le code ci-dessous
+
+$reponse = '<stong>D</strong>';
+echo '<br/> la réponse ultime c\'est la réponse $reponse' ;
+echo "<br/> la réponse ultime c\'est la réponse $reponse" ;
+
+
+
+
 
 ?>
